50fe072a103ab9587e0085f75951dbe32c52502e
Nestor López <lopezhdeznestor@gmail.com>
2016-10-23 01:29:31 -0500

    I created the generals files

--- yeiplan/public/resources/views/generals/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="page-footer white lighten-3 z-depth-2">
-    <div class="footer-copyright">
-        <div class="container">
-
-            <i class="cyan-text right">Animal's Care - © 2016, CodeCube. Derechos Reservados.</i>
-        </div>
-    </div>
-</footer>
